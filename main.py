b391e8e69488330b810166a2101f916a58e0ab4f
Nurlyssultan Sabitov <nurlyssultansabitov@Nurlyssultans-MacBook-Pro.local>
2023-08-16 13:55:39 +0600

    created a dummy project in Flask

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def get_index():
+    return render_template('index.html')
+
+
+@app.route('/blog')
+def get_blog():
+    return render_template('blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/portfolio')
+def get_portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route('/resume')
+def get_resume():
+    return render_template('resume.html')
